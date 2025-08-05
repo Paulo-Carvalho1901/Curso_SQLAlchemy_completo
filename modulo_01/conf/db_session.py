3bf74d0c7855bbed225da9828bcc8727fda07bca
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-05 16:03:48 -0300

    feito todos os imoports no db_session

---
+++ modulo_01/conf/db_session.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+
+from sqlalchemy.orm import sessionmaker
+
+from pathlib import Path
+from typing import Optional
+
+from sqlalchemy.orm  import Session
+from sqlalchemy.future.engine import Engine
+
+from models.model_base import ModelBase
+
